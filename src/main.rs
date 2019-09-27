cb3ae20b74c40147eae72ee342059f62eeda7643
Don Park <donp@donp.org>
2019-09-27 10:57:27 -0700

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,10 @@
-
-mod socket;
 mod config;
+mod socket;
 
 fn main() {
-    println!("Hello, world!");
+    println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
     let config_json = config::read("config.json").expect("config bad");
-    let mut config = config::new(&config_json);
-    println!("config {:?}", config);
-    //socket::open(config.host).expect("no sock");
-
-  // println!("pb codegen start");
-  // protobuf_codegen_pure::run(protobuf_codegen_pure::Args {
-  //     out_dir: "src/protos",
-  //     input: &["protos/contact.proto"],
-  //     includes: &["protos"],
-  //     customize: protobuf_codegen_pure::Customize {
-  //       ..Default::default()
-  //     },
-  // }).expect("protoc");
-  // println!("pb codegen stop");
+    let config = config::new(&config_json);
+    println!("{:?}", config);
+    socket::open(config.host).expect("no sock");
 }
--- src/socket.rs
+++ src/socket.rs
@@ -1,11 +1,14 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::str::FromStr;
+use std::time::Duration;
 
 pub fn open(host: &str) -> std::io::Result<()> {
-    let mut stream = TcpStream::connect(host)?;
+    let addr = SocketAddr::from_str("1.2.3.4:123").unwrap();
+    let mut stream = TcpStream::connect_timeout(&addr, Duration::new(5, 0))?;
 
     stream.write(&[1])?;
     stream.read(&mut [0; 128])?;
     Ok(())
 } // the stream is closed here
-
